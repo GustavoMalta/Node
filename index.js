daa181e79e31271487f0f25db9be921874a234cd
GustavoMalta <gustavo.a.malta@gmail.com>
2018-07-17 17:23:26 -0300

    Aula2

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
+var max = 10000;
+
 var serial = require('./serial');
 
-var valor1 = new serial.generate();
-var valor2 = new serial.generate();
+console.log(global);
 
-console.log("Index: " + serial.generate());
-console.log("valor1: " + valor1.);
+console.log("Index: " + serial);
+
+var valor1 = new serial();
+var valor2 = new serial();
+
+console.log("valor1: " + valor1);
 console.log("valor2: " + valor2);
 console.log (valor2 == valor1);
 
--- serial.js
+++ serial.js
@@ -1,10 +1,15 @@
+var createSerial = function(){
 
-var max = 10000;
+	var max = 10000;
 
-console.log("teste")
+	console.log("teste")
 
-this.generate = function(){
-	return Math.floor(Math.random()*max);
+	this.generate = function(){
+		return Math.floor(Math.random()*max);
+	};
+
 };
 
-console.log("serial: "+ this.generate());
+
+module.exports = createSerial;
+
